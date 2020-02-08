992dd36a30345046c1096c4d8a77503fadb8b9ff
jaskaran87 <jaskaransinghrajal@gmail.com>
2020-02-08 14:43:00 +0530

    add groupby

--- grouby_example/urls.py
+++ grouby_example/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import (
 						CityAdd,
+						PopulationSum
 				)
 
 urlpatterns = [
 	path('', CityAdd.as_view(), name = 'CityAdd'),
+	path('population', PopulationSum.as_view(), name="population")
+
 ]
--- grouby_example/views.py
+++ grouby_example/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.views.generic import ListView
 from .forms import CityForm
+from django.db.models import (Sum, Avg)
+from .models import City
 
 class CityAdd(View):
-	def get(self, request, *args, **kwargs):
-		Forms = CityForm() 
-		return render( 
-						request, 
-						'albums/add.html', 
-						{
-							'Forms' : Forms
-						} 
-					)
+    def get(self, request, *args, **kwargs):
+        Forms = CityForm() 
+        return render( 
+                        request, 
+                        'albums/add.html', 
+                        {
+                            'Forms' : Forms
+                        } 
+                    )
 
 
-	def post(self, request, *args, **kwargs):
-		Forms = CityForm(request.POST or None)
-		if Forms.is_valid():
-			Forms.save()
-			Forms = CityForm()
-		return render(request, 'albums/add.html', { 'Forms' : Forms })
+    def post(self, request, *args, **kwargs):
+        Forms = CityForm(request.POST or None)
+        if Forms.is_valid():
+            Forms.save()
+            Forms = CityForm()
+        return render(request, 'albums/add.html', { 'Forms' : Forms })
+
+
+class PopulationSum(ListView):
+    def get_queryset(self, *args, **kwargs):
+        return City.objects.values('country__name') \
+                    .annotate(country__population = Sum('population')) \
+                    .order_by('-country__population')
+    def get_template_names(self, *args, **kwargs):
+        return ['group_by/list.html']
+    
